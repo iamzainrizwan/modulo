c34cbca4578e71c67ef244c53782c156e1113688
zain <iamzainrizwan@gmail.com>
2026-05-20 22:20:20 +0100

    add command handlers and content

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var input = document.getElementById("command-input");
 var output = document.getElementById("output");
 
+input.focus();
+
 input.addEventListener('keydown', function(event){
   if (event.key == 'Enter') {
     var tokens = input.value.split(' ');
@@ -8,9 +10,22 @@ input.addEventListener('keydown', function(event){
     var response = "";
     switch (command) {
       case "whoami":
-        response = "zain rizwan. cool guy that doesn't know html for shit";
+        response = "zain - cs @ kcl (msci) · kcl cybersoc · coding tutor" + "<br>" + 
+                   "heading toward: sre / security engineering / distributed systems";
         break;
-    
+      
+      case "cat":
+        var parameter = tokens[1];
+        switch(parameter){
+          case "about.txt":
+            response = "home lab named after the library of alexandria - because if you're going to hoard knowledge, commit to the bit. currently running a node webapp and re::curse, occasionally catches fire (metaphorically). valhalla (razer blade 14 \'23, dual-boot) handles the rest. speakers don\'t work. it\'s fine (it\'s not)";
+            break;
+          default:
+            response = "file does not exist!";
+            break;
+        }
+        break;
+
       default:
         response = "whatever you typed was stupid lol";
         break;
@@ -19,6 +34,10 @@ input.addEventListener('keydown', function(event){
     input.value = "";
     output.scrollTop = output.scrollHeight;
   }
+});
+
+document.addEventListener('click', function(){
+  input.focus();
 });
 
 input.focus()
